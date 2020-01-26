2fe577859cd0d23aa2a753dcbe59e5b840d6dc79
Miguel C <miquecg@users.noreply.github.com>
2020-01-26 20:39:58 +0100

    Rename filter and function

--- containers/archlinux/filter_plugins/command.py
+++ containers/archlinux/filter_plugins/command.py
@@ -1,6 +1,6 @@
 from itertools import chain
 
-def to_list(options):
+def build_list(options):
     return list(chain.from_iterable(formatter(options)))
 
 def formatter(options):
@@ -13,5 +13,5 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'extra_opts': to_list
+            'opts_list': build_list
         }
