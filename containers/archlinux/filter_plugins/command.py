c76f83dae41e0162270c3e6d84d1d7b53601d85a
Miguel C <miquecg@users.noreply.github.com>
2020-01-26 21:23:56 +0100

    Refactor opts_list filter module

--- containers/archlinux/filter_plugins/command.py
+++ containers/archlinux/filter_plugins/command.py
@@ -1,13 +1,14 @@
 from itertools import chain
 
 def build_list(options):
-    return list(chain.from_iterable(formatter(options)))
+    names, values = map(formatter, options.keys()), options.values()
+    return flatten(zip(names, values))
 
-def formatter(options):
-    def long_option(opt):
-        return ('--{}'.format(opt))
+def formatter(opt):
+    return '--{}'.format(opt)
 
-    return zip(map(long_option, options.keys()), options.values())
+def flatten(nested):
+    return list(chain.from_iterable(nested))
 
 class FilterModule(object):
 
