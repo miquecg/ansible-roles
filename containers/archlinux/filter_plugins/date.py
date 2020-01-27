5a4aafa3b0988eeb3bdd1239c459f829e4aad14b
Miguel C <miquecg@users.noreply.github.com>
2020-01-27 20:14:11 +0100

    Make reasoning about date processing easier

--- containers/archlinux/filter_plugins/date.py
+++ containers/archlinux/filter_plugins/date.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 from functools import reduce, wraps
-from itertools import chain, repeat
 
 def formatted(func):
     @wraps(func)
@@ -12,21 +11,16 @@ def formatted(func):
     return date_with_format
 
 @formatted
-def move_date(date, month=0):
-    if month <= 0:
-        times = abs(month)
-        steps = backwards_month_iterator(times)
-        return reduce(lambda new_date, step_back: step_back(new_date), steps, date)
+def move_date(date, months_back=0):
+    new_month = change_month(date, months_back)
+    return change_day_to_first(new_month)
 
-def back_to_first_day(date):
+def change_month(date, months_back):
+    times = max(0, months_back)
+    return reduce(lambda new_date, _: change_day_to_first(new_date) - timedelta(days=1), range(times), date)
+
+def change_day_to_first(date):
     return date.replace(day=1)
-
-def subtract_one_day(date):
-    return date - timedelta(days=1)
-
-def backwards_month_iterator(times):
-    funcs = chain([[back_to_first_day]], repeat([subtract_one_day, back_to_first_day], times))
-    return chain.from_iterable(funcs)
 
 class FilterModule(object):
 
